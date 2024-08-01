bba9927597327e7b5db5fd30082ef35b34e1c653
Eugene Michkov <michkov.evgeny@gmail.com>
2024-08-01 20:36:37 +0300

    feature: get droplet ip

---
+++ src/requests/get-droplet-ip.ts
@@ -0,0 +1,20 @@
+import { ENV } from "@/config";
+
+export const getDropletIp = async (dropletId: number): Promise<string> => {
+  let response = await fetch(
+    `https://api.digitalocean.com/v2/droplets/${dropletId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${ENV.DIGITALOCEAN_TOKEN}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch droplet");
+  }
+
+  let data = await response.json();
+  return data.droplet.networks.v4[0].ip_address;
+};
